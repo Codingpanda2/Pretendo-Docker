db0b9823217d4465a58289c679ac85ca746c1d65
Codingpanda2 <stijnmud2010@hotmail.com>
2026-07-14 08:23:35 +0200

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const github = {
-    username: "MatthewL246",
+    username: "Codingpanda2",
     repo: "pretendo-docker",
 };
 
